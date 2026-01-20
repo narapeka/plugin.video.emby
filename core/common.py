d56b48539538fa1fb99affe800c9329bd0e3b348
narapeka <narapeka@hotmail.com>
2026-01-20 20:53:25 +0800

    fix nativemode is wrongly reset

--- core/common.py
+++ core/common.py
@@ -302,7 +302,11 @@ def set_path_filename(Item, ServerId, MediaSource, isDynamic=False):
         Item['KodiFilename'] = "unknown"
         NativeMode = False
 
-    if KodiPathLower.startswith("http://") or KodiPathLower.startswith("https://"):
+    if Container == 'iso' or KodiPathLower.endswith(".iso"):
+        NativeMode = True
+    elif KodiPathLower.startswith("dav://") or KodiPathLower.startswith("davs://"):
+        NativeMode = True
+    elif KodiPathLower.startswith("http://") or KodiPathLower.startswith("https://"):
         NativeMode = False
         Dynamic += "http/"
         isHttpByEmby = True
